4974ef5ccd19c01e39b9274ba6f9895cea924925
Spacedays <44454593+Spacedays@users.noreply.github.com>
2021-11-16 16:46:33 -0600

    Updates format to use Capital CamelCase for object variables/methods and lowercase camelCase for local vars

--- NBarLinkage.m
+++ NBarLinkage.m
@@ -1,101 +1,110 @@
 classdef NBarLinkage
     properties
-        linkage(2,:)        {mustBeReal} % First row is lengths, second row is angles [radians]; NaN for solution variables
-        drivingVar = [2,1]      % coordinate of driving variable
-        unknownPos1 = [2 2]     % Row, Col of unknown 1. First row is lengths, second is angles [rads]
-        unknownPos2 = [2 3]
-        priorGuesses = [0 0]    % Guess variables for fzero
-        opt                     % Fzero Options
+        Linkage    (2,:)        {mustBeReal} % First row is lengths, second row is angles [radians]; NaN for solution variables
+        DrivingVar (1,2) = [2,1]        % coordinate of driving variable
+        UnknownPos1(1,2) = [2 2]        % Row, Col of unknown 1. First row is lengths, second is angles [rads]
+        UnknownPos2(1,2) = [2 3]
+        LinkageRange  (2,:,:)               % used to store a range of linkage arrangements - row 1&2 are length& angle, columns are the variable numbers, 3 is the step index
+        PriorGuesses = [0 0]    % Guess variables for fzero
+        Opt                     % Fzero Options
     end
     % A four bar linkage will have two unknowns, one driving the other.
     
     methods
-        function obj = NBarLinkage(linkage,drivingVar,initGuesses)   % Constructor
+        function Obj = NBarLinkage(Linkage,DrivingVar,InitGuesses)   % Constructor
             % The unknows are defined by NaN values
             % Example fxn call: linkage = NBarLinkage([r1 r2 r3 r4; Th1 NaN Th3 NaN], [2,3], [10*D2R, 90*D2R]);
                 % Here, Th3 is the driving variable with Th2 and Th4 as the solution variables
             
             % Setup options for fsolve
-            obj.opt = optimset('Display','off');
+            Obj.Opt = optimset('Display','off');
             
             % Set obj.priorGuesses if provided
             if (nargin == 3)
-               obj.priorGuesses = initGuesses;
+               Obj.PriorGuesses = InitGuesses;
             end
             
             % Determine the array row & col of unknowns
-            unknowns = isnan(linkage);
+            unknowns = isnan(Linkage);
             if (length(unknowns(unknowns==1)) > 2)
                 disp("Too many unknowns in linkage matrix")
                 return
             end
-            obj.linkage = linkage;
-            obj.drivingVar = drivingVar;
+            Obj.Linkage = Linkage;
+            Obj.DrivingVar = DrivingVar;
             
             % ID unknown coordinates
             if (isempty(find(unknowns(1,:),1)))       % ==> Both unknowns are angles
                 unknownIndexes = find(unknowns(2,:));
-                obj.unknownPos1 = [2, unknownIndexes(1)];
-                obj.unknownPos2 = [2, unknownIndexes(2)];
+                Obj.UnknownPos1 = [2, unknownIndexes(1)];
+                Obj.UnknownPos2 = [2, unknownIndexes(2)];
             elseif (isEmpty(find(unknowns(2,:))))   % ==> Both unknowns are lengths
                 unknownIndexes = find(unknowns(2,:));
-                obj.unknownPos1 = [1, unknownIndexes(1)];
-                obj.unknownPos2 = [1, unknownIndexes(2)];
+                Obj.UnknownPos1 = [1, unknownIndexes(1)];
+                Obj.UnknownPos2 = [1, unknownIndexes(2)];
             else                                    % ==> One unk Angle, one unk Length
-                obj.unknownPos1 = [1, find(unknowns(1,:))];     %length
-                obj.unknownPos2 = [2, find(unknowns(2,:))];     %angle
+                Obj.UnknownPos1 = [1, find(unknowns(1,:))];     %length
+                Obj.UnknownPos2 = [2, find(unknowns(2,:))];     %angle
             end
         end
         
-        function out = LinkageEqn(obj, X)      % Function defined using geometry values from the ojb's linkage array
+        function out = LinkageEqn(Obj, X)      % Function defined using geometry values from the ojb's linkage array
             % Updates the unknowns' position to the previous guess
-            obj.linkage(obj.unknownPos1(1),obj.unknownPos1(2)) = X(1);%obj.priorGuesses(1);
-            obj.linkage(obj.unknownPos2(1),obj.unknownPos2(2)) = X(2);%obj.priorGuesses(2);
-            if (max(size(obj.linkage)) == 5)
-            eqn = obj.linkage(1,1).*exp(1i.*obj.linkage(2,1)) + obj.linkage(1,2).*exp(1i.*obj.linkage(2,2)) ...
-                 - obj.linkage(1,3).*exp(1i.*obj.linkage(2,3)) - obj.linkage(1,4).*exp(1i.*obj.linkage(2,4));
+            Obj.Linkage(Obj.UnknownPos1(1),Obj.UnknownPos1(2)) = X(1);%obj.priorGuesses(1);
+            Obj.Linkage(Obj.UnknownPos2(1),Obj.UnknownPos2(2)) = X(2);%obj.priorGuesses(2);
+            if (max(size(Obj.Linkage)) == 5)
+            eqn = Obj.Linkage(1,1).*exp(1i.*Obj.Linkage(2,1)) + Obj.Linkage(1,2).*exp(1i.*Obj.Linkage(2,2)) ...
+                 - Obj.Linkage(1,3).*exp(1i.*Obj.Linkage(2,3)) - Obj.Linkage(1,4).*exp(1i.*Obj.Linkage(2,4));
             end
-            if (max(size(obj.linkage)) == 4)
-            eqn = obj.linkage(1,1).*exp(1i.*obj.linkage(2,1)) + obj.linkage(1,2).*exp(1i.*obj.linkage(2,2)) ...
-                 - obj.linkage(1,3).*exp(1i.*obj.linkage(2,3)) - obj.linkage(1,4).*exp(1i.*obj.linkage(2,4));
+            if (max(size(Obj.Linkage)) == 4)
+            eqn = Obj.Linkage(1,1).*exp(1i.*Obj.Linkage(2,1)) + Obj.Linkage(1,2).*exp(1i.*Obj.Linkage(2,2)) ...
+                 - Obj.Linkage(1,3).*exp(1i.*Obj.Linkage(2,3)) - Obj.Linkage(1,4).*exp(1i.*Obj.Linkage(2,4));
             end
-            if (max(size(obj.linkage)) == 3)
-            eqn = obj.linkage(1,1).*exp(1i.*obj.linkage(2,1)) + obj.linkage(1,2).*exp(1i.*obj.linkage(2,2)) ...
-                 - obj.linkage(1,3).*exp(1i.*obj.linkage(2,3));
+            if (max(size(Obj.Linkage)) == 3)
+            eqn = Obj.Linkage(1,1).*exp(1i.*Obj.Linkage(2,1)) + Obj.Linkage(1,2).*exp(1i.*Obj.Linkage(2,2)) ...
+                 - Obj.Linkage(1,3).*exp(1i.*Obj.Linkage(2,3));
             end
             out = [real(eqn); imag(eqn)];
         end
         
-        function [unknown1, unknown2] = CalcLinkage(obj,drivingVarValue,drivingVar) % Use this function to calculate a linkage position
+        function out = LinkageBCEqn(Obj, X)      % Fxn using Obj.linkage and a boundary condition eqn
+            out = linkageEqn(Obj,X);
+            
+        end
+        
+        function MakeLinkageBCEqn()
+        
+        end
+        function [unknown1, unknown2] = CalcLinkage(Obj,drivingVarValue,DrivingVar) % Use this function to calculate a linkage position
             if (nargin < 2)
                 disp("ERROR: Not Enough Argiments")
                 return
             elseif (nargin > 2)
-                obj.drivingVar = drivingVar;
+                Obj.DrivingVar = DrivingVar;
             end
-            obj.linkage(obj.drivingVar(1),obj.drivingVar(2)) = drivingVarValue;     % Change driving Var Value as requested
+            Obj.Linkage(Obj.DrivingVar(1),Obj.DrivingVar(2)) = drivingVarValue;     % Change driving Var Value as requested
             
 %             fxn = ;
 %             a = @(X) LinkageEqn(obj,X);
-            Xtemp = fsolve(@(X) LinkageEqn(obj,X), obj.priorGuesses, obj.opt);
+            Xtemp = fsolve(@(X) LinkageEqn(Obj,X), Obj.PriorGuesses, Obj.Opt);
             unknown1 = Xtemp(1);
             unknown2 = Xtemp(2);
         end
         
-        function [solV1, solV2] = CalcLinkageRange(obj,drivingVarVector,options)
+        function [solV1, solV2] = CalcLinkageRange(Obj,drivingVarVector,options)
             % Returns an mxn vector of the entire linkage for the analysis range
             arguments
-                obj
-                drivingVarVector (1,:) {mustBeNumeric}
-                options.drivingVar (1,2) {mustBeNumeric}
+                Obj
+                drivingVarVector (1,:) {mustBeNumeric}      
+                options.DrivingVar (1,2) {mustBeNumeric}    
                 options.FullSoltn (1,1) {mustBeNumeric} = 0
             end
             
             if (nargin < 2)
                 disp("ERROR: Not Enough Argiments")
                 return
-            elseif (nargin > 2 && ~isempty(options.drivingVar))
-                obj.drivingVar = options.drivingVar;
+            elseif (nargin > 2 && ~isempty(options.DrivingVar))
+                Obj.DrivingVar = options.DrivingVar;
             end
             
             
@@ -103,13 +112,13 @@ classdef NBarLinkage
             solVectors = ones(2,length(drivingVarVector));
                 for k=1:(length(drivingVarVector))
                     % Solving for Position
-                    drivingVarValue = drivingVarVector(k);
-                    [sol1,sol2] = CalcLinkage(obj,drivingVarValue);
+                    DrivingVarValue = drivingVarVector(k);
+                    [sol1,sol2] = CalcLinkage(Obj,DrivingVarValue);
                     solVectors(1,k) = sol1;                  % Store solutions to vector
                     solVectors(2,k) = sol2;
 
-                    obj.priorGuesses(1) = sol1;    % Use last solution for next guess
-                    obj.priorGuesses(2) = sol2;
+                    Obj.PriorGuesses(1) = sol1;    % Use last solution for next guess
+                    Obj.PriorGuesses(2) = sol2;
                 end
                 
             if ~options.FullSoltn
@@ -117,26 +126,26 @@ classdef NBarLinkage
                 solV2 = solVectors(2,:);
             else
                 % Setup initial vectors
-                rVectors = ones(length(obj.linkage), length(drivingVarVector));
-                thVectors = ones(length(obj.linkage), length(drivingVarVector));
-                for k=1:length(obj.linkage)
-                    rVectors(k,:) = rVectors(k) .* obj.linkage(1,k);
-                    thVectors(k,:) = thVectors(k) .* obj.linkage(2,k);
+                rVectors = ones(length(Obj.Linkage), length(drivingVarVector));
+                thVectors = ones(length(Obj.Linkage), length(drivingVarVector));
+                for k=1:length(Obj.Linkage)
+                    rVectors(k,:) = rVectors(k) .* Obj.Linkage(1,k);
+                    thVectors(k,:) = thVectors(k) .* Obj.Linkage(2,k);
                 end
                 
-                %solVectors = CalcSoltnRange(obj,drivingVarVector);
+                %solVectors = CalcSoltnRange(obj,DrivingVarVector);
                 % Put the solution vectors in the right place
-                switch obj.unknownPos1(1)
+                switch Obj.UnknownPos1(1)
                     case 1
-                        rVectors(obj.unknownPos1(2),:) = solVectors(1,:);
+                        rVectors(Obj.UnknownPos1(2),:) = solVectors(1,:);
                     case 2
-                        thVectors(obj.unknownPos1(2),:) = solVectors(1,:);
+                        thVectors(Obj.UnknownPos1(2),:) = solVectors(1,:);
                 end
-                switch obj.unknownPos2(1)
+                switch Obj.UnknownPos2(1)
                     case 1
-                        rVectors(obj.unknownPos2(2),:) = solVectors(2,:);
+                        rVectors(Obj.UnknownPos2(2),:) = solVectors(2,:);
                     case 2
-                        thVectors(obj.unknownPos2(2),:) = solVectors(2,:);
+                        thVectors(Obj.UnknownPos2(2),:) = solVectors(2,:);
                 end
                 solV1 = rVectors;
                 solV2 = thVectors;
@@ -145,5 +154,69 @@ classdef NBarLinkage
             
         end
 
+        function [solV1, solV2] = CalcChangingLinkage(Obj,drivingLinkageVector,options)
+            % Returns an mxn vector of the entire linkage for the analysis range
+            arguments
+                Obj
+                drivingLinkageVector (:,:) {mustBeNumeric}      % MxN vector of M links over range N, with two vectors of NaN
+                options.DrivingVar (1,2) {mustBeNumeric}        % Allows changing the driving variable on the object
+                options.FullSoltn (1,1) {mustBeNumeric} = 0
+            end
+            
+            if (nargin < 2)
+                disp("ERROR: Not Enough Argiments")
+                return
+            elseif (nargin > 2 && ~isempty(options.DrivingVar))
+                Obj.DrivingVar = options.DrivingVar;
+            end
+            
+            Obj.LinkageRange = drivingLinkageVector;
+            
+            drivingVarVector = Obj.LinkageRange(Obj.DrivingVar(1), Obj.DrivingVar(2), :);
+                for k=1:(length(drivingVarVector))
+                    % Solving for Position
+                    [sol1,sol2] = CalcLinkage(Obj,drivingVarVector(k));
+                    Obj.LinkageRange(Obj.UnknownPos1(1),Obj.UnknownPos1(2),k) = sol1;                  % Store solutions to vector
+                    Obj.LinkageRange(Obj.UnknownPos2(1),Obj.UnknownPos2(2),k) = sol2;   % solVectors(2,k) = sol2;
+
+                    Obj.PriorGuesses(1) = sol1;    % Use last solution for next guess
+                    Obj.PriorGuesses(2) = sol2;
+                end
+                
+            if ~options.FullSoltn           %!!!
+                solV1 = Obj.LinkageRange(Obj.UnknownPos1(1),Obj.UnknownPos1(2),:);
+                solV2 = Obj.LinkageRange(Obj.UnknownPos2(1),Obj.UnknownPos2(2),:);
+            else
+                solV1 = Obj.LinkageRange(1,:,:);
+                solV2 = Obj.LinkageRange(2,:,:);
+%                 % Setup initial vectors
+%                 rVectors = ones(length(Obj.Linkage), length(drivingLinkageVector));
+%                 thVectors = ones(length(Obj.Linkage), length(drivingLinkageVector));
+%                 for k=1:length(Obj.Linkage)
+%                     rVectors(k,:) = rVectors(k) .* Obj.Linkage(1,k);
+%                     thVectors(k,:) = thVectors(k) .* Obj.Linkage(2,k);
+%                 end
+%                 
+%                 %solVectors = CalcSoltnRange(obj,DrivingVarVector);
+%                 % Put the solution vectors in the right place
+%                 switch Obj.UnknownPos1(1)
+%                     case 1
+%                         rVectors(Obj.UnknownPos1(2),:) = solVectors(1,:);
+%                     case 2
+%                         thVectors(Obj.UnknownPos1(2),:) = solVectors(1,:);
+%                 end
+%                 switch Obj.UnknownPos2(1)
+%                     case 1
+%                         rVectors(Obj.UnknownPos2(2),:) = solVectors(2,:);
+%                     case 2
+%                         thVectors(Obj.UnknownPos2(2),:) = solVectors(2,:);
+%                 end
+%                 solV1 = rVectors;
+%                 solV2 = thVectors;
+            end
+            
+            disp("Done")
+        end
+        
     end
 end
